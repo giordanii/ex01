e36927001abea12af12f52285a990c576e06832c
João Giordani <jgiordanii@gmail.com>
2023-03-13 12:05:27 -0300

    commit

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let num1 = Number(window.prompt('Choose a number'));
+let num2 = Number(window.prompt('Choose another number'));
+let ok = window.alert('Choose the operator');
+let list = window.document.getElementById('list')
+let operation = '';
+let result = '';
+
